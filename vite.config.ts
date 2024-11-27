47da01395b24028b8a45279faf29e8b7ba728317
Abdoul Ahad Mbacke DIOUF <abdoul.diouf2@groupe-esigelec.org>
2024-11-27 20:50:25 +0100

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio_last_version/', // Ensure this matches your GitHub Pages subdirectory
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
